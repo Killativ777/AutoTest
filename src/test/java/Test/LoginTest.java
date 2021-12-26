11bf2efa8c0850355df4f47a2d2ea754bec6edb8
Killativ777 <killativspidey@gmail.com>
2021-12-26 16:12:23 +0200

    All in one test. Webelements removed from methods.

--- src/test/java/Test/LoginTest.java
+++ src/test/java/Test/LoginTest.java
@@ -4,28 +4,28 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class LoginTest {
     public WebDriver driver;
+
     public LoginTest(WebDriver driver){
         PageFactory.initElements(driver, this);
         this.driver=driver;
+
     }
-    public void clickLogin(){
-        WebElement loginBtn = driver.findElement(By.xpath("//a[@class='userbar__button __active']"));
-        loginBtn.click();
-    }
-    public void inputLogin(){
-        WebElement enterLogin = driver.findElement(By.xpath("//*[@id=\"login_form_id\"]/dl/dd[1]/input"));
-        enterLogin.sendKeys("killativspidey@gmail.com");
-    }
-    public void inputPassword(){
-        WebElement enterPassword = driver.findElement(By.xpath("//*[@id=\"login_form_id\"]/dl/dd[2]/input"));
-        enterPassword.sendKeys("Gimno3000");
-    }
-    public void clickBtnLogin(){
-            WebElement btnLogin = driver.findElement(By.xpath("//*[@id=\"login_form_id\"]/dl/dd[3]/span[1]/input"));
-            btnLogin.click();
-    }
+    @FindBy (xpath = "//a[@class='userbar__button __active']")
+    WebElement loginBtn;
+    @FindBy (xpath = "//input[@class='field j-focus'][@tabindex='1']")
+    WebElement enterLogin;
+    @FindBy (xpath = "//input[@type='password'][@class='field'][@tabindex='2']")
+    WebElement enterPassword;
+    @FindBy (xpath = "//input[@class = 'btn-input'][@value ='Увійти']")
+    WebElement btnLogin;
+
+    public void clickLogin(){loginBtn.click();}
+    public void inputLogin(){enterLogin.sendKeys("killativspidey@gmail.com");}
+    public void inputPassword(){enterPassword.sendKeys("Gimno3000");}
+    public void clickBtnLogin(){btnLogin.click();}
 }
